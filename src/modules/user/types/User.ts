d52e5d485a7267fa74cd7ecf09ed73707ca9d723
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2025-09-27 21:25:58 +0100

    fix build

--- src/modules/user/types/User.ts
+++ src/modules/user/types/User.ts
@@ -1,21 +1,12 @@
 import { Types } from 'mongoose';
 import { AuthProviders } from '../enums/AuthProviders.enum';
-import { WorkoutSplit } from 'src/modules/workout/types/WorkoutSplit';
-import { UserTokenUsage } from './UserTokenUsage';
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Settings {}
 
 export interface UserType {
   _id: Types.ObjectId;
   name: string;
   email: string;
   password?: string;
-  workoutSplits: WorkoutSplit[];
-  currentWorkoutSplitId?: string;
-  settings: Settings;
-  tokenUsage: UserTokenUsage;
-  totalWorkouts: number;
+
   surname?: string;
   firstName?: string;
   authId?: string;
@@ -29,5 +20,4 @@ export interface UserType {
   country: string;
   createdAt?: Date;
   updatedAt?: Date;
-  isFirstLogin: boolean;
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,28 +3,13 @@ import { UserController } from './controllers/user.controller';
 import { UserService } from './services/user.service';
 import { User, UserSchema } from './schemas/User.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Workout, WorkoutSchema } from '../workout/schema/Workout.schema';
-import { UsageLimitService } from './services/monthly-usage/monthly-usage.service';
-import {
-  PersonalRecord,
-  PersonalRecordSchema,
-} from '../personal-records/schemas/PersonalRecord.schema';
-import {
-  WorkoutProcessing,
-  WorkoutProcessingSchema,
-} from '../workout/schema/WorkoutProcessing.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: User.name, schema: UserSchema },
-      { name: Workout.name, schema: WorkoutSchema },
-      { name: PersonalRecord.name, schema: PersonalRecordSchema },
-      { name: WorkoutProcessing.name, schema: WorkoutProcessingSchema },
-    ]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UserController],
-  providers: [UserService, UsageLimitService],
-  exports: [UserService, UsageLimitService],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
